f5ec58753250ada1bdfa5240d68d6ac95b346122
Michelle-P95 <michelle.poetsch@gmx.de>
2021-10-28 11:24:30 +0200

    add Menu Buttons

---
+++ Assets/Scripts/MenuManger.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManger : MonoBehaviour
+{
+    public GameObject menu;
+
+    bool gameIsPaused = false;
+    //bool timeAktiv = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Return();
+            }
+            else
+            {
+                menu.SetActive(true);
+                Time.timeScale = 0;
+                gameIsPaused = true;
+            }
+        }
+    }
+
+    public void Return()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    // Starting MENU
+    public void StartGame()
+    {
+       // SceneManager.LoadScene()  //TODO load Game
+    }
+
+    public void Help()
+    {
+        //TODO load Text "du musst alle vasen kaputt machen"
+    }
+
+    public void Quit()
+    {
+        
+    }
+
+    public void Settings()
+    {
+       // TODO UI settings
+    }
+
+
+}
