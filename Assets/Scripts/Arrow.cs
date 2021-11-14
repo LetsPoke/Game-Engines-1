2839e4d3c8ad26cab4ddee533ecbdb7e381d6ca3
LetsPoke <59581396+LetsPoke@users.noreply.github.com>
2021-11-14 20:02:35 +0100

    fixed shoot downways

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -6,10 +6,17 @@ public class Arrow : MonoBehaviour
 {
     private AudioSource sound;
     private GameObject player;
+    private GameObject health;
+    private Collider2D co2;
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        health = GameObject.FindGameObjectWithTag("healthItem");
+
         sound = player.GetComponent<AudioSource>();
+        co2 = GetComponent<Collider2D>();
+        Physics2D.IgnoreCollision(co2, player.GetComponent<Collider2D>(), true);
+        Physics2D.IgnoreCollision(co2, health.GetComponent<Collider2D>(), true);
     }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("breakable")) {
