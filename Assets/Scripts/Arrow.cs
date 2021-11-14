22daa38bd7ba128c94cdfbcea8f539338091f2a5
Markmc17 <64312949+Markmc17@users.noreply.github.com>
2021-11-14 18:45:49 +0100

    Update

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    private AudioSource sound;
+    private GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        sound = player.GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("breakable")) {
             collision.GetComponent<pot>().Smash();
+            sound.Play();
         }
     }
 
--- Assets/Scripts/pot.cs
+++ Assets/Scripts/pot.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class pot : MonoBehaviour
 {
     private Animator anim;
-    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,7 +25,7 @@ public class pot : MonoBehaviour
 
     IEnumerator breakCo() 
     {
-        audioSource.Play();
+        
         yield return new WaitForSeconds(.3f);
         this.gameObject.SetActive(false);
         
