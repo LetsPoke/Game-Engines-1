8e09f007d7ba8ba5bc5711117c7b95c75ea47622
LetsPoke <59581396+LetsPoke@users.noreply.github.com>
2021-11-28 19:35:07 +0100

    merged everything

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI TimerText;
+    public float startTime;
+    private bool started = false;
+    private bool finnished = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerText = GetComponent<TextMeshProUGUI>();
+        StartTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (started)
+        {
+            if (finnished){
+                return;
+            }
+                
+
+            float t =  startTime - Time.time;
+
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f0");
+
+            TimerText.text = minutes + ":" + seconds;
+
+             if (t <= 0)
+            {
+                finnished = true;
+            }
+        }
+    }
+
+    public void StartTime() 
+    {
+        started = true;
+    }
+
+    public void Finnish()
+    {
+        finnished = true;
+        TimerText.fontSize = 60;
+        TimerText.color = Color.green;
+    }
+}
